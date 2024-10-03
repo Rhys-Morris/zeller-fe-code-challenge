d9a3c5b90f6a81493562be33acb2f558c873deee
Rhys Morris <rhysmorris08@gmail.com>
2024-10-03 10:48:52 +1000

    feat: Add wrapper and section styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,27 @@ import { CustomerRow } from "./components/customer/CustomerRow";
 import { RoleRadios } from "./components/RoleRadios";
 import { useCustomers } from "./hooks/useCustomers";
 import { titleCase } from "./utils/string";
-import "./App.module.css";
+import styles from "./App.module.css";
 
 function App() {
   const { customersByRole, isError } = useCustomers();
   const [selectedRole, setSelectedRole] = useState<Role>("ADMIN");
   const customers = customersByRole.get(selectedRole) ?? [];
 
-  console.log(customersByRole);
-
   return (
-    <div>
-      <div>
-        <section>
-          <h2>User Types</h2>
+    <div className={styles.wrapper}>
+      <div className={styles.sectionContainer}>
+        <section className={styles.section}>
+          <h2 className={styles.sectionHeader}>User Types</h2>
           <RoleRadios
             selectedRole={selectedRole}
             setSelectedRole={setSelectedRole}
           />
         </section>
-        <section>
-          <h2>{titleCase(selectedRole)} Users</h2>
+        <section className={styles.section}>
+          <h2 className={styles.sectionHeader}>
+            {titleCase(selectedRole)} Users
+          </h2>
           {customers.map((customer) => (
             <CustomerRow customer={customer} key={customer.id} />
           ))}
--- src/api/customers/service.ts
+++ src/api/customers/service.ts
@@ -6,7 +6,6 @@ import { Customer, CustomerResponseSchema } from "./schema";
 export class CustomerService {
   static async getCustomers(): Promise<Customer[]> {
     const result = await postQuery(AllCustomersQuery);
-    console.log("RESULT", result);
     return validateApiResponse(CustomerResponseSchema, result.data)
       .listZellerCustomers.items;
   }
